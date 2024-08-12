a8b53c2c909ed8a777a7c9371875deaa83e65b87
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2024-08-12 17:19:15 -0300

    Agrego Nuevo codigo de prueba

--- CodigosDeClases/bubble_sort.py
+++ CodigosDeClases/bubble_sort.py
@@ -1,1 +1,15 @@
-# Archivo donde agregare codigo que voy probando de la teoria.
+# Archivo donde agregare codigo que voy probando de la teoria y investigacion del tema a estudiar.
+
+# Bubble sort
+
+def BubbleSort(lista):
+    for numPasada in range(len(lista)-1,0,-1):
+        for i in range(numPasada):
+            if lista[i]>lista[i+1]:
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+
+lista = [54,26,93,17,77,31,44,55,20]
+ordenamiento = BubbleSort(lista)
+print(lista)
