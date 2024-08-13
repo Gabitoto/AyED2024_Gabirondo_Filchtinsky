25142831a7adb708eeb1ac661d953ce5bc7c1c39
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2024-08-13 15:05:53 -0300

    Actualizacion de codigo usado en clases

--- CodigosDeClases/bubble_sort.py
+++ CodigosDeClases/bubble_sort.py
@@ -13,3 +13,23 @@ def BubbleSort(lista):
 lista = [54,26,93,17,77,31,44,55,20]
 ordenamiento = BubbleSort(lista)
 print(lista)
+
+
+# Bubble Sort Corto
+
+def ordenamientoBurbujaCorto(unaLista):
+    intercambios = True
+    numPasada = len(unaLista)-1
+    while numPasada > 0 and intercambios:
+       intercambios = False
+       for i in range(numPasada):
+           if unaLista[i]>unaLista[i+1]:
+               intercambios = True
+               temp = unaLista[i]
+               unaLista[i] = unaLista[i+1]
+               unaLista[i+1] = temp
+       numPasada = numPasada-1
+
+unaLista=[20,30,40,90,50,60,70,80,100,110]
+ordenamientoBurbujaCorto(unaLista)
+print(unaLista)
