fdab75c06bd0edff73e54003bcafe2510bde8d97
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2024-08-20 22:03:42 -0300

    Avances en la teoria, se prueba codigo de recursion y memoizacion

--- CodigosDeClases/TAD.py
+++ CodigosDeClases/TAD.py
@@ -3,7 +3,7 @@
 # Factorial
 
 def factorial(n):
-    # En esta funcion aplicamos recursion para poder resolver el factoria lde un numero.
+    # En esta funcion aplicamos recursion para poder resolver el factorial de un numero.
     if n>1:
         n = n * factorial(n-1)
         return n
@@ -14,3 +14,44 @@ def factorial(n):
     
 objeto = factorial(5)
 print(objeto)
+
+# Serie Fibonacci: Este enfoque es más intuitivo, pero tiene una complejidad de tiempo exponencial, lo que significa que se vuelve ineficiente para valores grandes de N.
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        n = fibonacci(n-1) + fibonacci(n-2)
+        return n
+
+for i in range(10):
+    print(fibonacci(i), end=" ")
+    
+# Memoizacion: La memoización es una técnica de optimización utilizada principalmente en programación dinámica y algoritmos recursivos para mejorar la eficiencia de las funciones que calculan resultados repetidos.
+
+def Fibonacci_memo(n, memo={}):
+    """Cuando una función recursiva se llama con los mismos argumentos múltiples veces, la memoización guarda los resultados de estas llamadas en una estructura de datos (como un diccionario en Python). En lugar de recalcular el resultado cada vez, la función simplemente devuelve el valor almacenado en el diccionario si ya se ha calculado anteriormente."""
+    if n in memo:
+        return memo[n]
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        memo[n] = Fibonacci_memo(n-1, memo) + Fibonacci_memo(n -2, memo)
+        return memo[n]
+    
+print(Fibonacci_memo(10))
+
+# Estructuras de Datos.
+
+"""class Listasimp:
+    def __init__(self,inicio,espacio):
+        self.inicio = None
+        self.espacio = None
+    
+    def agregar(self,n):
+        pass"""
+        
