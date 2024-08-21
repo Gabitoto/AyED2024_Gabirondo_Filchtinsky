f296173b012bc7a363fb5a1500f069ee911512ea
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2024-08-21 18:07:31 -0300

    Avances en actividad numero 2 TP1

--- TrabajoPractico_1/proyecto_1/modules/modulo2.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo2.py
@@ -1,2 +1,20 @@
 # Actividad numero 2: TAD Lista Doblemente Enlazada
 
+# Clase Nodo la cual va tener 3 componentes: el valor, la referencia al siguiente nodo y una referencia al anterior nodo.
+
+class Node:
+    
+    def __init__(self,valor):
+        self.value = valor
+        self.next = None
+        self.prev = None
+
+# Clase ListaDobleEnlazada desde donde se manejaran las acciones o metodos.
+
+class ListaDobleEnlazda:
+    
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        
+        
