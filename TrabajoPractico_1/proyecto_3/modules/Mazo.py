2891c1867184a67b2051b0d5ebe9848fa46d42eb
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2024-09-05 17:07:08 -0300

    Agrego dockstring con una breve explicacion de las funciones de la clase Mazo

--- TrabajoPractico_1/proyecto_3/modules/Mazo.py
+++ TrabajoPractico_1/proyecto_3/modules/Mazo.py
@@ -1,27 +1,33 @@
 
 
-from Carta import Carta
+from Carta import Carta  
 from LDE import ListaDobleEnlazada
 
+# Explicacion de un DequeEmptyError(Exception) = Es una excepción personalizada que se lanza cuando intentas realizar una operación en un deque (doblemente terminado) vacío, como extraer un elemento cuando no hay ninguno disponible.
 class DequeEmptyError(Exception):
-    """Un jugador se ha quedado sin cartas"""
     def __init__(self,message = "Un jugador se ha quedado sin cartas"):
         self.massage = message
 
 class Mazo():
     def __init__ (self):
+        """Se busca que el mazo actue como una LDE y posea sus metodos y atributos"""
         self.mazo = ListaDobleEnlazada()
         
     def poner_carta_arriba(self, carta):
+        """Colocamos una carta al inicio del mazo por lo que se utiliza la funcion del la LDE: Agregar_al_inicio."""
         self.mazo.agregar_al_inicio(carta)
         self.cabeza = carta
             
     def poner_carta_abajo(self,carta):
+        """Colocamos una carta abajo del mazo por lo que ademas de agregarla al final debemos setearla para que no sea visible."""
         carta.visible = False
         self.mazo.agregar_al_final(carta)
         self.cola = carta
     
     def sacar_carta_arriba(self):
+        """La función primero verifica que el mazo no esté vacío y que la cabeza del mazo no sea None.
+        Si estas condiciones se cumplen, crea un objeto Carta,lo marca como visible (ya que se está mostrando) y lo retorna.
+        Si no hay más cartas, lanza una excepción."""
         if self.mazo.tamanio > 0 and self.mazo.cabeza is not None:
             carta = self.mazo.extraer(0)
             carta.visible = True
@@ -31,6 +37,7 @@ class Mazo():
         
         
     def __len__ (self):
+        """Llamamos a la funcion de la LDE que nos permite utilizar el metodo len() sobre nuestro mazo."""
         return self.mazo.__len__()
 
 if __name__ == "__main__":             
