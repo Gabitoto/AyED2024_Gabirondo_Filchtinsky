50a930910a9f3be7ea0e92660fa8db69e061dad9
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2024-09-10 17:38:27 -0300

    Corrijo el archivo de medidas

--- TrabajoPractico_1/proyecto_2/tests/mediciones_LDE.py
+++ TrabajoPractico_1/proyecto_2/tests/mediciones_LDE.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 15 13:02:03 2022
-
-@author: jinsfran
-
-Medición de tiempos de ejecución y graficación
-    Escriba un programa que permita medir el tiempo de ejecución de una función que obtiene todas las combinaciones de una lista de n números creados al azar tomando los números de a r valores. El programa debe medir el tiempo de ejecución para los valores de r en el rango de 1 a n.
-    Para encontrar las combinaciones, puede utilizar la función “combinations” del módulo “itertools” o crear su propio algoritmo.
-    El programa debe crear un gráfico que muestre el tiempo de ejecución versus r. El gráfico debe tener título, leyendas en ambos ejes, una grilla y un marcador que indique el máximo tiempo.
-    Indicar, analizando la gráfica el valor de r para el cual el tiempo de ejecución es mayor.
-"""
 from modules.LDE import ListaDobleEnlazada
 import random
 import time
